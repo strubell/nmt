ea13cccf9b3e77ba2f6b8f0f1f6be915d41a5ef8
Emma Strubell <emma.strubell@gmail.com>
2018-12-20 14:50:16 -0500

    debug

--- nmt/utils/iterator_utils.py
+++ nmt/utils/iterator_utils.py
@@ -189,8 +189,8 @@ def get_iterator(src_dataset,
                         # todo: uncomment
                         # tf.concat(([tgt_sos_id], tgt), 0),
                         # tf.concat((tgt, [tgt_eos_id]), 0)),
-                        tf.concat((tf.expand_dims([tf.constant(sos), tf.constant(sos)]), tgt), 0),
-                        tf.concat((tgt, tf.expand_dims([tf.constant(eos), tf.constant(eos)], 0)), 0)),
+                        tf.concat((tf.constant(sos, shape=[1, 2]), tgt), 0),
+                        tf.concat((tgt, tf.constant(eos, shape=[1, 2])), 0)),
       num_parallel_calls=num_parallel_calls).prefetch(output_buffer_size)
   # Add in sequence lengths.
   if use_char_encode:
