b112318cb17aa092033c60a4420b41658771fa2c
Emma Strubell <emma.strubell@gmail.com>
2018-12-20 11:11:51 -0500

    debug

--- nmt/utils/iterator_utils.py
+++ nmt/utils/iterator_utils.py
@@ -144,7 +144,7 @@ def get_iterator(src_dataset,
 
   src_tgt_dataset = src_tgt_dataset.map(
     lambda src, tgt: (
-      tf.string_split([src], delimiter="_").values, tf.string_split([tgt], delimiter="_").values),
+      tf.string_split(src, delimiter="_").values, tf.string_split(tgt, delimiter="_").values),
     num_parallel_calls=num_parallel_calls).prefetch(output_buffer_size)
 
   # Filter zero length input sequences.
